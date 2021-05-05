d38d03fcefbc35a2560ab0f0f32610238ecc6349
han-lab <houhan@gmail.com>
2021-05-05 00:47:42 -0400

    Apr21 version (left choice and bitcode start overlapping bug...)

--- pipeline_utils/ConvertBitcode.m
+++ pipeline_utils/ConvertBitcode.m
@@ -8,23 +8,34 @@ end
 % In line with my pybpod code
 bitCodeDigits = 20;
 
-chan.protocol = 1;  % Protocol dig marker channel
+chan.protocol = 2;  % Protocol dig marker channel
 eventMarkerDur.bitcodeEachbit = 1;
 eventMarkerDur.goCue = 10;
 eventMarkerDur.reward = 20;
+eventMarkerDur.ITI = 30;
 
-chan.behavior = 0;  % Behavior dig marker channel
-eventMarkerDur.choiceL = 1;
-eventMarkerDur.choiceR = 2;
+chan.behavior = 2;  % Behavior dig marker channel (I moved all behavior channel to bitcode channel in Apr 21 version)
+eventMarkerDur.choiceL = 2;   % I made a mistake in Apr version that choiceL and the start of bitcode are the SAME....
+eventMarkerDur.choiceR = 3;
 
 %% get Ephys Bitcode
 % Start of a trial (onset of my bitcode is indicated by twice of the bitcode width)
 sTrig = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.bitcodeEachbit * 2)); 
+
+% Solve the bitcode-start and choiceL overlapping bug...
+ITI = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.ITI));  
+choiceL = [];
+for i=1:length(ITI)
+    this_choice_idx = find(sTrig(i) < sTrig & sTrig < ITI(i));
+    choiceL = [choiceL; sTrig(this_choice_idx)];
+    sTrig(this_choice_idx) = [];
+end
+
 eTrig=[sTrig(2:end); sTrig(end)+20];
 bitsAll = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.bitcodeEachbit)); 
 goCue = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.goCue)); 
 reward = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.reward)); 
-choiceL = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceL)); 
+% choiceL = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceL)); 
 choiceR = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceR)); 
 
 bitcode = zeros(length(sTrig), bitCodeDigits);
@@ -42,13 +53,13 @@ imecFolders = dir(fullfile(sessionDir, '*imec*'));
 for f = 1:length(imecFolders)  % Save the same bitcode.mat to each imec folder 
     toSave = [imecFolders(f).name(1 : strfind(imecFolders(f).name,'imec')-1) 'bitcode.mat'];
     save(fullfile(imecFolders(f).folder, imecFolders(f).name, toSave), ...
-        'bitcode', 'bitCodeS', 'goCue', 'sTrig', 'reward', 'choiceL', 'choiceR');
+        'bitcode', 'bitCodeS', 'goCue', 'sTrig', 'reward', 'choiceL', 'choiceR', 'ITI');
 end
 
 function txtFile = getTxtFileName(sessionDir, chan, duration)
 txtFile = dir(fullfile(sessionDir, sprintf('*%g_%g.adj.txt', chan, duration)));  % Try TPrime adjusted first
 if isempty(txtFile)  % If no adj.txt, try raw txt
-    txtFile = dir(fullfile(sessionDir, sprintf('*%g_%g.txt', chan, duration)));  % Try TPrime adjusted first    
+    txtFile = dir(fullfile(sessionDir, sprintf('*%g_%g.txt', chan, duration)));
 end    
 txtFile = fullfile(txtFile.folder, txtFile.name);
     
