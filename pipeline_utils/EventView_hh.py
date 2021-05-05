053c4a446002de1e1d0bdc420ec60fabfe76f347
han-lab <houhan@gmail.com>
2021-05-05 06:08:01 -0400

    EventPlugin: automatically switch plot styles

--- pipeline_utils/EventView_hh.py
+++ pipeline_utils/EventView_hh.py
@@ -23,7 +23,11 @@ axis_list = []
 plot_handles = []
 
 STRIG_, GOCUE_, CHOICEL_, CHOICER_, REWARD_, ITI_ = 0, 1, 2, 3, 4, 5
-line_styles = ['-', '--', ':', '-.']
+styles_single_color = [['-', 2], ['-', 0.5], ['--', 1], [':', 1], ['-.', 1]]
+styles_multiple_colors = [[[1.0000, 0.0078, 0.0078], '-', 2],
+                          [[0.03137, 0.5725, 0.9882], '-', 2],
+                          [[1.0000, 0.0078, 0.0078], '--', 0.5],
+                          [[0.03137, 0.5725, 0.9882], '--', 0.5]]
 
 
 def _make_default_colormap():
@@ -35,6 +39,52 @@ def _make_default_colormap():
     colormap[0] = [0.03137, 0.5725, 0.9882]
     colormap[1] = [1.0000, 0.0078, 0.0078]
     return colormap
+
+
+def load_bitcode_mat():  # Directly load and parse mat file
+
+    # Load bitcode.mat
+    mat = scipy.io.loadmat(glob.glob('*bitcode.mat')[0])
+    dig_marker_per_trial = mat['digMarkerPerTrial']
+    # Trial types
+    ignore_trials = np.all(np.isnan(dig_marker_per_trial[:, [CHOICEL_, CHOICER_]]), 1)
+    reward_trials = ~np.isnan(dig_marker_per_trial[:, REWARD_])
+    noreward_trials = ~ignore_trials & ~reward_trials
+    L_trials = ~np.isnan(dig_marker_per_trial[:, CHOICEL_])
+    R_trials = ~np.isnan(dig_marker_per_trial[:, CHOICER_])
+    events = {}
+
+    # ----- Define events times ------
+
+    # 1. Choice_direction
+    events['choice_direction'] = {'Left': mat['choiceL'], 'Right': mat['choiceR']}
+
+    # 2. Choice_outcome
+    choice_times = np.nanmean(dig_marker_per_trial[:, [CHOICEL_, CHOICER_]], 1)
+    choice_reward = choice_times[reward_trials]
+    choice_noreward = choice_times[noreward_trials]
+    events['choice_outcome'] = {'reward': choice_reward, 'no_reward': choice_noreward}
+
+    # 3. Gocue_outcome
+    gocue_reward = dig_marker_per_trial[reward_trials, GOCUE_]
+    gocue_noreward = dig_marker_per_trial[noreward_trials, GOCUE_]
+    gocue_ignore = dig_marker_per_trial[ignore_trials, GOCUE_]
+    events['gocue_outcome'] = {'reward': gocue_reward, 'no_reward': gocue_noreward, 'ignore': gocue_ignore}
+
+    # iti_reward = dig_marker_per_trial[reward_trials, ITI_]
+    # iti_noreward = dig_marker_per_trial[noreward_trials, ITI_]
+    # iti_ignore = dig_marker_per_trial[ignore_trials, ITI_]
+    # events['iti_outcome'] = {'reward': iti_reward, 'no_reward': iti_noreward, 'ignore': iti_ignore}
+
+    # 4. ITI_choice*outcome
+    iti_L_reward = dig_marker_per_trial[reward_trials & L_trials, ITI_]
+    iti_L_noreward = dig_marker_per_trial[noreward_trials & L_trials, ITI_]
+    iti_R_reward = dig_marker_per_trial[reward_trials & R_trials, ITI_]
+    iti_R_noreward = dig_marker_per_trial[noreward_trials & R_trials, ITI_]
+    events['iti_choice_outcome'] = {'L_reward': iti_L_reward, 'R_reward': iti_R_reward,
+                                    'L_noreward': iti_L_noreward, 'R_noreward': iti_R_noreward}
+
+    return events
 
 
 class EventView(ManualClusteringView):
@@ -50,7 +100,7 @@ class EventView(ManualClusteringView):
         self.nodata = False
 
         try:
-            self.events_all = self.load_bitcode_mat()
+            self.events_all = load_bitcode_mat()
             self.n_event_plots = len(self.events_all)
             # events = genfromtxt('events.csv', delimiter=',')
         except OSError:
@@ -103,37 +153,48 @@ class EventView(ManualClusteringView):
 
         for i, d in enumerate(cluster_ids):    # For each cluster
             for j, (event_plot_name, event_plot_events) in enumerate(self.events_all.items()):      # For each event plot
+                # Prepare
                 raster_top = 0
+                axis_psth = axis_list[i * (2 * self.n_event_plots) + j]
+                axis_raster = axis_list[i * (2 * self.n_event_plots) + j + self.n_event_plots]
+
+                axis_psth.axvline(x=0, color='white', alpha=.5)
 
                 for k, (events_name, events_this) in enumerate(event_plot_events.items()):   # For each line in each plot
                     # Get data
                     rasters, activity, yrast, ntrials, nevents = self.get_spikes(d, events_this)
-                    hist, bins = np.histogram(activity, weights=np.ones(nevents) * (50 / ntrials), range=(-5, 5), bins=250)
+                    hist, bins = np.histogram(activity, weights=np.ones(nevents) * (50 / ntrials), range=(-5, 5),
+                                              bins=250)
+                            
+                    if len(cluster_ids) > 1:   # If more than one clusters are selected, only use the cluster's color
+                        this_style, this_lw = styles_single_color[k % len(styles_single_color)]
+                        this_color = self.cmap[i]
+                    else:    # If only one cluster is selected, use Svoboda lab's convention (e.g., red for L, blue for R)
+                        this_color, this_style, this_lw = styles_multiple_colors[k % len(styles_multiple_colors)]
 
-                    # Prepare
-                    this_style = line_styles[k % len(line_styles)]
-                    axis_psth = axis_list[i * (2 * self.n_event_plots) + j]
-                    axis_raster = axis_list[i * (2 * self.n_event_plots) + j + self.n_event_plots]
+                    psth_style = {'color': this_color, 'linestyle': this_style, 'lw': this_lw}
+                    raster_style = {'color': [this_color]}
 
                     # Plot psth
-                    axis_psth.plot(bins[:-1], hist, color=self.cmap[i], linestyle=this_style)
-                    axis_psth.axvline(x=0, color='white', alpha=.5)
-                    axis_psth.set_xticks(np.linspace(-2, 3, 9))
-                    axis_psth.set_xlim(left=-2, right=3)
+                    axis_psth.plot(bins[:-1], hist, label=events_name, **psth_style)
+                    # axis_psth.set_xticks(np.linspace(-2, 3, 9))
 
                     # Plot raster
-                    axis_raster.scatter(rasters, yrast + raster_top, c=[self.cmap[i]],
-                                        marker='|', s=np.ones(len(rasters)) * .4, alpha=0.8)
-                    axis_raster.axvline(x=0, color='white', alpha=.5)
+                    axis_raster.scatter(rasters, yrast + raster_top,
+                                        marker='o', s=np.ones(len(rasters)) * .4, alpha=0.8, **raster_style)
                     raster_top += ntrials
                     axis_raster.axhline(y=raster_top, color='white', alpha=.5)
 
-                    # Axis settings
-                    axis_raster.set_ylim(bottom=0, top=raster_top)
-                    axis_psth.set_ylim(bottom=0, top=None)
-                    self.fix_axis(axis_psth, 10)
-                    self.fix_axis(axis_raster, 10)
-
+                # Axis settings
+                axis_psth.set_xlim(left=-2, right=3)
+                axis_psth.set_title(event_plot_name, fontsize=20)
+                axis_psth.legend(loc='upper left')
+                axis_psth.set_ylim(bottom=0, top=None)
+                axis_raster.axvline(x=0, color='white', alpha=.5, lw=1)
+                axis_raster.set_ylim(bottom=0, top=raster_top)
+                axis_raster.invert_yaxis()
+                self.fix_axis(axis_psth, 15)
+                self.fix_axis(axis_raster, 15)
 
         print(ttime, 'plotting')
         t = time.time()
@@ -175,38 +236,6 @@ class EventView(ManualClusteringView):
                 yrast = np.append(yrast, np.ones(len(temp)) * i)
                 activity.extend(temp)
         return rasters, np.array(activity), yrast, np.amax(yrast), len(activity)
-
-    def load_bitcode_mat(self):  # Directly load and parse mat file
-
-        # Load bitcode.mat
-        mat = scipy.io.loadmat(glob.glob('*bitcode.mat')[0])
-        dig_marker_per_trial = mat['digMarkerPerTrial']
-        # Trial types
-        ignore_trials = np.all(np.isnan(dig_marker_per_trial[:, [CHOICEL_, CHOICER_]]), 1)
-        reward_trials = ~np.isnan(dig_marker_per_trial[:, REWARD_])
-        noreward_trials = ~ignore_trials & ~reward_trials
-        events = {}
-
-        # Define events times
-        choice_L, choice_R = mat['choiceL'], mat['choiceR']
-        events['choice_direction'] = {'Left': choice_L, 'Right': choice_R}
-
-        gocue_reward = dig_marker_per_trial[reward_trials, GOCUE_]
-        gocue_noreward = dig_marker_per_trial[noreward_trials, GOCUE_]
-        gocue_ignore = dig_marker_per_trial[ignore_trials, GOCUE_]
-        events['gocue_outcome'] = {'reward': gocue_reward, 'no_reward': gocue_noreward, 'ignore': gocue_ignore}
-
-        choice_times = np.nanmean(dig_marker_per_trial[:, [CHOICEL_, CHOICER_]], 1)
-        choice_reward = choice_times[reward_trials]
-        choice_noreward = choice_times[noreward_trials]
-        events['choice_outcome'] = {'reward': choice_reward, 'no_reward': choice_noreward}
-
-        iti_reward = dig_marker_per_trial[reward_trials, ITI_]
-        iti_noreward = dig_marker_per_trial[noreward_trials, ITI_]
-        iti_ignore = dig_marker_per_trial[ignore_trials, ITI_]
-        events['iti_outcome'] = {'reward': iti_reward, 'no_reward': iti_noreward, 'ignore': iti_ignore}
-
-        return events
 
 
 class EventPlugin(IPlugin):
