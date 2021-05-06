51dcc7e10d2128c74550363a10c0dae2b171042c
han-lab <houhan@gmail.com>
2021-05-06 01:29:16 -0400

    for Mar21 bitcode version

--- pipeline_utils/ConvertBitcode.m
+++ pipeline_utils/ConvertBitcode.m
@@ -8,62 +8,70 @@ end
 % In line with my pybpod code
 bitCodeDigits = 20;
 
-chan.protocol = 2;  % Protocol dig marker channel
+chan.protocol = 1;  % Protocol dig marker channel
 eventMarkerDur.bitcodeEachbit = 1;
 eventMarkerDur.goCue = 10;
 eventMarkerDur.reward = 20;
-eventMarkerDur.ITI = 30;
+eventMarkerDur.ITI = 0;
 
-chan.behavior = 2;  % Behavior dig marker channel (I moved all behavior channel to bitcode channel in Apr 21 version)
-eventMarkerDur.choiceL = 2;   % I made a mistake in Apr version that choiceL and the start of bitcode are the SAME....
-eventMarkerDur.choiceR = 3;
+chan.behavior = 0;  % Behavior dig marker channel (I moved all behavior channel to bitcode channel in Apr 21 version)
+eventMarkerDur.choiceL = 1;   % I made a mistake in Apr version that choiceL and the start of bitcode are the SAME....
+eventMarkerDur.choiceR = 2;
 
 %% get Ephys Bitcode
 % Start of a trial (onset of my bitcode is indicated by twice of the bitcode width)
 sTrig = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.bitcodeEachbit * 2)); 
 
 % Solve the bitcode-start and choiceL overlapping bug...
-iti = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.ITI));  
-choiceL = [];
-for i=1:length(iti)
-    this_choice_idx = find(sTrig(i) < sTrig & sTrig < iti(i));
-    choiceL = [choiceL; sTrig(this_choice_idx)];
-    sTrig(this_choice_idx) = [];
-end
+iti_all = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.ITI));  
+% choiceL = [];
+% for i=1:length(iti)
+%     this_choice_idx = find(sTrig(i) < sTrig & sTrig < iti(i));
+%     choiceL = [choiceL; sTrig(this_choice_idx)];
+%     sTrig(this_choice_idx) = [];
+% end
 
-eTrig=[sTrig(2:end); sTrig(end)+20];
 bitsAll = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.bitcodeEachbit)); 
 goCue = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.goCue)); 
 reward = dlmread(getTxtFileName(sessionDir, chan.protocol, eventMarkerDur.reward)); 
-% choiceL = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceL)); 
+choiceL = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceL)); 
 choiceR = dlmread(getTxtFileName(sessionDir, chan.behavior, eventMarkerDur.choiceR)); 
+
+iti = setdiff(iti_all, [bitsAll; goCue; reward; sTrig]);
 
 bitcode = zeros(length(sTrig), bitCodeDigits);
 
 [STRIG_, GOCUE_, CHOICEL_, CHOICER_, REWARD_, ITI_] = deal(1,2,3,4,5,6);
-digMarkerPerTrial = nan(length(sTrig), 6);  % [STrig, goCue, choiceL, choiceR, reward, ITI】
-digMarkerPerTrial(:,[STRIG_, GOCUE_, ITI_]) = [sTrig, goCue, iti];  % Must exists 
 
-for i = 1:length(sTrig)  % For each trial
+% Use iti as trial marker to exclude truncated trials
+digMarkerPerTrial = nan(length(iti), 6);  % [STrig, goCue, choiceL, choiceR, reward, ITI]
+
+% digMarkerPerTrial(:,[STRIG_, GOCUE_, ITI_]) = [sTrig, goCue, iti];  % Must exists 
+digMarkerPerTrial(:,ITI_) = iti;  % Must exists 
+
+for i = 1:length(iti)  % For each trial
+    thisStart = sTrig(find(sTrig < iti(i), 1, 'last'));  % Deal with truncated trials
+    digMarkerPerTrial(i, STRIG_) = thisStart;
+    digMarkerPerTrial(i, GOCUE_) = goCue(thisStart < goCue & goCue < iti(i));
+    
     % Parse bitcode
-    bitHighThis = bitsAll(bitsAll>sTrig(i) & bitsAll<eTrig(i));
+    bitHighThis = bitsAll(thisStart < bitsAll & bitsAll < iti(i));
     bitHighPositionThis = round((bitHighThis - sTrig(i) - eventMarkerDur.bitcodeEachbit * 1e-3) / (2 * eventMarkerDur.bitcodeEachbit * 1e-3));
     bitcode(i, bitHighPositionThis) = 1;
     
     % Fill in the digMarkerPerTrial matrix
-    thisL = choiceL(sTrig(i) < choiceL & choiceL < iti(i));
+    thisL = choiceL(thisStart < choiceL & choiceL < iti(i));
     if ~isempty(thisL)
         digMarkerPerTrial(i, CHOICEL_) = thisL;
     end
-    thisR = choiceR(sTrig(i) < choiceR & choiceR < iti(i));
+    thisR = choiceR(thisStart < choiceR & choiceR < iti(i));
     if ~isempty(thisR)
         digMarkerPerTrial(i, CHOICER_) = thisR;
     end        
-    thisRew = reward(sTrig(i) < reward & reward < iti(i));
+    thisRew = reward(thisStart < reward & reward < iti(i));
     if ~isempty(thisRew)
         digMarkerPerTrial(i, REWARD_) = thisRew;
     end             
-    
 end
 
 bitCodeS = num2str(bitcode, '%d');
