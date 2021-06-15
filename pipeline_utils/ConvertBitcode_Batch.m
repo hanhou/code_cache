321f25ae044122dd8ed0427bf575dd82df8b9c72
han-lab <houhan@gmail.com>
2021-06-15 18:15:35 -0400

    batch ConvertBitcode_Batch.m

---
+++ pipeline_utils/ConvertBitcode_Batch.m
@@ -0,0 +1,15 @@
+%% Batch convert txt files from catGT to bitcode.mat files for phy and ingestion
+rootPath = 'F:\catGT';
+allSubFolder = dir(fullfile(rootPath, '**\*.*'));
+allSubFolder = allSubFolder([allSubFolder.isdir]);
+allSubFolder = allSubFolder(arrayfun(@(A)contains(A.name,'catgt'), allSubFolder));
+
+%%
+for f = 1:length(allSubFolder)
+    thisFolder = fullfile(allSubFolder(f).folder, allSubFolder(f).name);
+    try
+        ConvertBitcode(thisFolder);
+    catch
+        fprintf('!!! Errors in processing %s', thisFolder);
+    end
+end
