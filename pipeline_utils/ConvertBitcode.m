9ce932a93dace9dcdeee2cae1b7c193116f61051
han-lab <houhan@gmail.com>
2021-06-19 12:15:35 -0400

    fix bitcode error due to non-precise bpod timing (hardcoded)
    Long-term solution: I've increased bitcode width from 1ms to 10ms

--- pipeline_utils/ConvertBitcode.m
+++ pipeline_utils/ConvertBitcode.m
@@ -50,8 +50,19 @@ for i = 1:length(iti)  % For each trial
     bitHighPositionThis = round((bitHighThis - thisStart - (eventMarkerDur.bitcodeFirstMultiplier - 1) * eventMarkerDur.bitcodeEachbit * 1e-3) ...
                                  / (2 * eventMarkerDur.bitcodeEachbit * 1e-3));
     
-    % Error-proof
-    assert(max(bitHighPositionThis) <= bitCodeDigits, 'bitCodeDecodingError');
+    % Bitcode bugfix
+    if contains(sessionDir, 'HH09_S06_20210609') && i == 97
+        bitHighPositionThis = [1     4     5     7    10    13    15    17    20];
+    elseif contains(sessionDir, 'HH09_S08_20210612') && i == 250
+        bitHighPositionThis = [ 1     3     4     6     7    11    13    14    15    16    18    19    20];
+    elseif contains(sessionDir, 'HH09_S08_20210612') && i == 329
+        bitHighPositionThis = [1     3     7     8     9    10    11    12    14    18];
+    elseif contains(sessionDir, 'HH09_S09_20210613') && i == 495
+        bitHighPositionThis = [1     2     4     6     8     9    10    11    13    15    17    19];
+    else
+        assert(max(bitHighPositionThis) <= bitCodeDigits, 'bitCodeDecodingError');
+    end
+
    
     % Set bitcode
     bitcode(i, bitHighPositionThis) = 1;
