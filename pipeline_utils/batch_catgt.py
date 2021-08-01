4dde03ca8bcc2b7e516f29a9e92495e6242af893
han-lab <houhan@gmail.com>
2021-08-01 19:34:48 -0400

    print no file found

--- pipeline_utils/batch_catgt.py
+++ pipeline_utils/batch_catgt.py
@@ -13,7 +13,7 @@ import subprocess
 
 # ========= Settings ==========
 catgt_exe = r'D:\Han_Sync\Svoboda\Scripts\Ephys\CatGT_2.0\CatGT.exe'
-data_root_path = r'h:\HH100' 
+data_root_path = r'h:' 
 
 # If empty, output files will be stored in the original folders,
 # otherwise, in conventional CatGt folders structure.
@@ -41,24 +41,28 @@ for imec in imec_folders:
     if any(ex in imec for ex in exclude_keywords):
         continue
 
-    # Retrieve run, g, prb
-    m = re.search(r'(\w+)_g(\d+)_imec(\d+)', imec)
-    run, g, prb = m.group(1), m.group(2), m.group(3)
+    # Retrieve dir, run, g, prb
+    m = re.search(r'(.*)\\(.*)\\(\w+)_g(\d+)_imec(\d+)', imec)
+    if not m:
+        print(f' !! Errors in parsing folder {imec}')
+        continue
+    dir, run, g, prb = m.group(1), m.group(3), m.group(4), m.group(5)
 
     # Retrieve t_max
     files = glob.glob(imec + '/*g*_t*.ap.bin') 
     if not files:    # Must have at least one .ap.bin file
+        print(f' !! No ap.bin found for {imec}')
         continue
     ts = [int(re.search(r'\w+_g\d+_t(\d+).*', file).group(1)) for file in files]
     t_max = max(ts)
 
     # Do catGT
-    cmd = f'{catgt_exe} -dir={data_root_path} -prb_fld -run={run} -g={g} -prb={prb} -t=0,{t_max} {catgt_cmd} {output_cmd}'
+    cmd = f'{catgt_exe} -dir={dir} -prb_fld -run={run} -g={g} -prb={prb} -t=0,{t_max} {catgt_cmd} {output_cmd}'
     err = subprocess.call(cmd.split())  # Not using split(' ') to take care of more than one spaces between strings
 
     if not err:
         n_probe += 1
-        print(f'{n_probe} done: {imec}, t_max = {t_max}')
+        print(f'{n_probe} done: {dir}, {run}, imec{prb}, g{g}, t_max = {t_max}')
     else:
         print(f' !! Errors in processing {imec}, t_max = {t_max}')
 
